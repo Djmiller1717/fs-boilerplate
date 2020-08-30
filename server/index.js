e265042ad66c876d846e1b7d2d57ee637907dec3
Doug Miller <Djmiller1717@gmail.com>
2020-08-29 23:56:19 -0400

    updated template

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 const express = require("express")
+const path = require('path')
 //initialize app
 const app = express()
 //require morgan|volleyball, path packages
 const morgan = require('morgan')
 //require db from /db
-const {db} = require('./db')
+
 
 //use morgan|volleyball
 app.use(morgan('dev'))
 //use express.json()
-app.use(expres.json())
+app.use(express.json())
 //use express.static() MAKE SURE THE PATH TO YOUR PUBLIC FOLDER IS RIGHT!
 app.use('/public', express.static(path.join(__dirname, 'public')))
 //app.use('/public', express.static(path.join(__dirname, 'server/public')))
@@ -27,7 +28,7 @@ const PORT = 3000
 const init = async function(){
     //await syncAndSeed()
     app.listen(PORT, function(){
-        console.log(`Server is listening o port ${PORT}`)
+        console.log(`Server is listening on port ${PORT}`)
     })
 }
 
